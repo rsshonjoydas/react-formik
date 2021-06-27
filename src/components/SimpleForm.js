e6f161020124991340b3e6a719960de628c9dba9
rsshonjoydas <rsshonjoydas@gmail.com>
2021-06-27 19:01:34 +0600

    email validation RegEx

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -2,16 +2,39 @@
 import { useFormik } from 'formik';
 import React from 'react';
 
+const initialValues = {
+  name: 'RS Shonjoy',
+  email: '',
+  channel: '',
+};
+
+const onSubmit = (values) => {
+  console.log('Form data', values);
+};
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.name) {
+    errors.name = 'Required';
+  }
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email format';
+  }
+  if (!values.channel) {
+    errors.channel = 'Required';
+  }
+
+  return errors;
+};
+
 const SimpleForm = () => {
   const formik = useFormik({
-    initialValues: {
-      name: 'RS Shonjoy',
-      email: '',
-      channel: '',
-    },
-    onSubmit: (values) => {
-      console.log('Form data', values);
-    },
+    initialValues,
+    onSubmit,
+    validate,
   });
 
   // console.log('Form values', formik.values);
