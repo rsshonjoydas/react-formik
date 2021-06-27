f4e5433809e1aa9f21fb0ed70ca39af0e904c297
rsshonjoydas <rsshonjoydas@gmail.com>
2021-06-27 18:58:22 +0600

    Managing form state

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -1,21 +1,48 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
+import { useFormik } from 'formik';
 import React from 'react';
 
-const SimpleForm = () => (
-  <form>
-    <label htmlFor="name">Name</label>
-    <input type="text" id="name" name="name" />
+const SimpleForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: 'RS Shonjoy',
+      email: '',
+      channel: '',
+    },
+  });
 
-    <label htmlFor="email" id="email" name="name">
-      E-mail
-    </label>
-    <input type="email" id="email" name="email" />
+  console.log('Form values', formik.values);
+  return (
+    <form>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
 
-    <label htmlFor="channel">Channel</label>
-    <input type="text" id="channel" name="channel" />
+      <label>E-mail</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
 
-    <button type="submit">Submit</button>
-  </form>
-);
+      <label htmlFor="channel">Channel</label>
+      <input
+        type="text"
+        id="channel"
+        name="channel"
+        onChange={formik.handleChange}
+        value={formik.values.channel}
+      />
 
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 export default SimpleForm;
