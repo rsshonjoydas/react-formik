1f4692af7f77cad6f8d31a9b02aa8ce3a2e328ee
rsshonjoydas <rsshonjoydas@gmail.com>
2021-07-16 14:49:21 +0600

    select option formik control

--- src/components/FormikControl/FormikContainer.js
+++ src/components/FormikControl/FormikContainer.js
@@ -4,13 +4,21 @@ import * as Yup from 'yup';
 import FormikControl from './FormikControl';
 
 const FormikContainer = () => {
+  const dropdownOptions = [
+    { key: 'Select an option', value: '' },
+    { key: 'Option 1', value: 'options1' },
+    { key: 'Option 2', value: 'options2' },
+    { key: 'Option 3', value: 'options3' },
+  ];
   const initialValues = {
     email: '',
     description: '',
+    selectOption: '',
   };
   const validationSchema = Yup.object({
     email: Yup.string().required('Required'),
     description: Yup.string().required('Required'),
+    selectOption: Yup.string().required('Required'),
   });
   const onSubmit = (values) => console.log('Form data', values);
   return (
@@ -19,6 +27,12 @@ const FormikContainer = () => {
         <Form>
           <FormikControl control="input" type="email" label="Email" name="email" />
           <FormikControl control="textarea" label="Description" name="description" />
+          <FormikControl
+            control="select"
+            label="Select a topic"
+            name="selectOption"
+            options={dropdownOptions}
+          />
           <button type="submit">Submit</button>
         </Form>
       )}
---
+++ src/components/FormikControl/Select.js
@@ -0,0 +1,22 @@
+import { ErrorMessage, Field } from 'formik';
+import React from 'react';
+import TextError from './TextError';
+
+function Select(props) {
+  const { label, name, options, ...rest } = props;
+  return (
+    <div className="formControl">
+      <label htmlFor={name}>{label}</label>
+      <Field as="select" id={name} name={name} {...rest}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.key}
+          </option>
+        ))}
+      </Field>
+      <ErrorMessage name={name} component={TextError} />
+    </div>
+  );
+}
+
+export default Select;
