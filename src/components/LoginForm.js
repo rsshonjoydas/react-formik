7b8a328c91cfced9065258e8c9ac87a7b0509ddb
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-17 01:53:48 +0600

    Login Form

---
+++ src/components/LoginForm.js
@@ -0,0 +1,35 @@
+import { Form, Formik } from 'formik';
+import React from 'react';
+import * as Yup from 'yup';
+import FormikControl from './FormikControl/FormikControl';
+
+const LoginForm = () => {
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().email('Invalid email format!').required('Required'),
+    password: Yup.string().required('Required'),
+  });
+
+  const onSubmit = (values) => {
+    console.log('Form data', values);
+  };
+  return (
+    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+      {(formik) => (
+        <Form>
+          <FormikControl control="input" type="email" label="Email" name="email" />
+          <FormikControl control="input" type="password" label="Password" name="password" />
+          <button type="submit" disabled={!formik.isValid}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default LoginForm;
